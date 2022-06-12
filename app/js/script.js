5d7c6a15579a1dd9df675417e56ce86d37d47926
zeynab <zeynab.mvs@gmail.com>
2022-06-12 11:16:05 +0430

    header>navbar desktop done

---
+++ app/js/script.js
@@ -0,0 +1,27 @@
+const btnHamburgur = document.querySelector('#jsbtnHamburgur')
+const header = document.querySelector('.header')
+const overlay = document.querySelector('.overlay')
+const fadeElems = document.querySelectorAll('.has-fade')
+const body = document.querySelector('body')
+
+btnHamburgur.addEventListener('click', function(){ 
+    console.log(fadeElems)
+    
+    if(header.classList.contains('open')){ // Close hamburgur menu
+        body.classList.remove('noscroll')
+        header.classList.remove('open')
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-in')
+            element.classList.add('fade-out')
+        });
+    } 
+    else { // Open hamburgur menu
+        body.classList.add('noscroll')
+        header.classList.add('open') 
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-out')
+            element.classList.add('fade-in')
+            
+        });
+    } 
+})
